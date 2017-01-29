b403ab65ad45981e5e7c64b79b560b5d02093391
mokkoko <5richs@gmail.com>
2017-01-28 19:26:38 -0700

    Initial commit

---
+++ program.py
@@ -0,0 +1,25 @@
+import random
+
+print('-----------------------------------------')
+print('         GUESS THAT NUMBER GAME')
+print('-----------------------------------------')
+print()
+
+the_number = random.randint(0, 100)
+
+guess = -1
+
+name = input('Player what is your name? ')
+
+while guess != the_number:
+    guess_text = input('Guess a number between 0 and 100: ')
+    guess = int(guess_text)
+
+    if guess < the_number:
+        print('Sorry {1}, Your guess of {0} was too LOW'.format(guess, name))
+    elif guess > the_number:
+        print('Sorry {1}, Your guess of {0} was too HIGH'.format(guess, name))
+    else:
+        print('Excellent work {}, you won, it was, {}!'.format(name, guess))
+
+print('done')
